fab016764af55b93fc418e8bd2cfefaa99d1aa70
007ebey <ebeypra@hotmail.com>
2025-08-04 23:49:57 +0530

    update

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/style/images/favicon.png">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/style/images/S.png">
   <title><?php bloginfo('name'); ?><?php wp_title('|', true, 'left'); ?></title>
   <?php wp_head(); ?>
 </head>
