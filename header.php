babad923d791b6720639099237364633ef6b4d5a
007ebey <ebeypra@hotmail.com>
2025-08-06 10:59:22 +0530

    logo

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/style/images/S.png">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/style/images/A_1x.png">
   <title><?php bloginfo('name'); ?><?php wp_title('|', true, 'left'); ?></title>
   <?php wp_head(); ?>
 </head>
