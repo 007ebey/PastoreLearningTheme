6e12e4fe7844fa6019b7d5921f307d24511aa693
007ebey <ebeypra@hotmail.com>
2025-08-15 18:29:34 +0530

    login

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/style/images/A_1x.png">
-  <title><?php bloginfo('name'); ?><?php wp_title('|', true, 'left'); ?></title>
+  <title><?php bloginfo('name'); ?> 1.0<?php wp_title('|', true, 'left'); ?></title>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class('full-layout'); ?>>
